8872d9aaf8b8b9e6c1cdc17996c7ef9df1ebb5ff
Brian Pontarelli <brian@inversoft.com>
2013-11-07 13:26:42 -0700

    Removed plugin dir - not used

--- test-deps/plugins/org/savantbuild/plugins/groovy/1.0/src/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-jar -cvf ../groovy-1.0.jar plugin.savant
-cd ..
-md5sum groovy-1.0.jar | awk -F' ' '{print $1}' > groovy-1.0.jar.md5
--- test-deps/plugins/org/savantbuild/plugins/settings/1.0/src/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-jar -cvf ../settings-1.0.jar plugin.savant
-cd ..
-md5sum settings-1.0.jar | awk -F' ' '{print $1}' > settings-1.0.jar.md5
