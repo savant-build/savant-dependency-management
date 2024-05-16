425b379b1a7560a771eda223a48e2342eb2b0fcc
Daniel DeGroff <daniel@fusionauth.io>
2024-05-16 15:01:57 -0600

    Script cleanup

--- src/main/shell/update-license-list-data.sh
+++ src/main/shell/update-license-list-data.sh
@@ -1,13 +1,17 @@
 
-mkdir -p build/license-list-data
-git clone --depth=1 git@github.com:spdx/license-list-data.git build/license-list-data
-rm -rf src/main/resources/license-details
-rm -rf src/main/resources/license-exceptions
-rm src/main/resources/exceptions.json
-rm src/main/resources/licenses.json
-cp -Rp build/license-list-data/json/details src/main/resources/license-details
-cp -Rp build/license-list-data/json/exceptions src/main/resources/license-exceptions
-cp -p build/license-list-data/json/exceptions.json src/main/resources/exceptions.json
-cp -p build/license-list-data/json/licenses.json src/main/resources/licenses.json
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+base=$(echo $script_dir | sed -r 's/(savant-dependency-management).*/\1/')
+echo $base
+
+mkdir -p $base/build/license-list-data
+git clone --depth=1 git@github.com:spdx/license-list-data.git $base/build/license-list-data
+rm -rf $base/src/main/resources/license-details
+rm -rf $base/src/main/resources/license-exceptions
+rm $base/src/main/resources/exceptions.json
+rm $base/src/main/resources/licenses.json
+cp -Rp $base/build/license-list-data/json/details $base/src/main/resources/license-details
+cp -Rp $base/build/license-list-data/json/exceptions $base/src/main/resources/license-exceptions
+cp -p $base/build/license-list-data/json/exceptions.json $base/src/main/resources/exceptions.json
+cp -p $base/build/license-list-data/json/licenses.json $base/src/main/resources/licenses.json
 
 
